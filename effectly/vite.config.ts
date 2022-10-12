17fca7743b5c0ead6590a7b712767fbe70f04806
wooczak <kacper.luczak99@gmail.com>
2022-10-12 18:29:30 +0200

    Vitest: installation & configuration

--- effectly/vite.config.ts
+++ effectly/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    test: {
+        // Config for test coverage - set to 80% for all, using Istanbul
+        coverage: {
+            provider: "istanbul",
+            reporter: ["text", "json", "html"],
+            lines: 80,
+            functions: 80,
+            branches: 80
+        },
+    },
+});
