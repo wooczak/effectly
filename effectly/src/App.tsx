7a62268aece92412e406607bc0ec972099752b86
wooczak <kacper.luczak99@gmail.com>
2022-10-20 12:52:31 +0200

    Test commit

--- effectly/src/App.tsx
+++ effectly/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          This is a test. count is {count}
+          This is a yet another test. count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
