334604aacf4b8b768b2d4dcb4f24c9c38a281db7
solTheW <patrick.marentine@gmail.com>
2022-10-20 21:44:31 +0200

    Firestore connection

---
+++ effectly/src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyDy9Ufx3skb3hYjAU7NoOJZjIuyfvb9ZFU',
+  authDomain: 'effectly-frb.firebaseapp.com',
+  projectId: 'effectly-frb',
+  storageBucket: 'effectly-frb.appspot.com',
+  messagingSenderId: '216387321584',
+  appId: '1:216387321584:web:7ab5dbb5d2c0b58ecf4c35',
+  measurementId: 'G-ZYEB5WGRYG',
+}
+
+const app = initializeApp(config)
+
+export const database = getFirestore(app)
