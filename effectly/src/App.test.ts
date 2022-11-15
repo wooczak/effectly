f3ef57c9c53fe4d524e54eb50af7811f042a9fd1
wooczak <kacper.luczak99@gmail.com>
2022-11-15 19:44:53 +0100

    Creating global style for the app

--- effectly/src/App.test.ts
+++
@@ -1,8 +0,0 @@
-// Test file for Vitest
-import { describe, expect, it } from "vitest";
-
-describe("WHEN the user uses a simple function", () => {
-    it("THEN should return the correct result", () => {
-        expect(2 + 2).toEqual(4);
-    });
-});
---
+++ effectly/src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,15 @@
+import GlobalStyle from "../../core/style/globalStyle";
+import { Header, Wrapper } from "./SignIn.styles";
+
+const SignIn = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <Wrapper>
+                <Header>effectly</Header>
+            </Wrapper>
+        </>
+    );
+};
+
+export default SignIn;
