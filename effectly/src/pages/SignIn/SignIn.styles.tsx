e2bf207a41ff47f2ec8cd448f491c00b8b43b76d
Kacper Luczak <kacper.luczak99@gmail.com>
2022-12-22 12:23:06 +0100

    Poszerzenie szerokosci wrappera

--- effectly/src/pages/SignIn/SignIn.styles.tsx
+++ effectly/src/pages/SignIn/SignIn.styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.section`
     border-radius: 10px;
-    width: 600px;
+    width: 800px;
     top: 50%;
     left: 50%;
     position: absolute;
