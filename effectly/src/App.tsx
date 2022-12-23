bd040b56183df337bdee3f7436b91806c5ba0722
Kacper Luczak <kacper.luczak99@gmail.com>
2022-12-23 09:53:26 +0100

    Fixing bugs

--- effectly/src/App.tsx
+++ effectly/src/App.tsx
@@ -3,7 +3,7 @@ import useCurrentUser from "./hooks/global/useCurrentUser";
 const App = () => {
   const currentUser = useCurrentUser();
 
-  return <p>{`Main app, welcome ${currentUser.email}`}</p>;
+  return <p>{`Main app, welcome ${currentUser?.email}`}</p>;
 };
 
 export default App;
