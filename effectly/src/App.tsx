dc084b857b8a273d5b758f75c8a3abc26c99833a
Kacper Luczak <kacper.luczak99@gmail.com>
2022-12-27 16:50:01 +0100

    Adding sign out button to test

--- effectly/src/App.tsx
+++ effectly/src/App.tsx
@@ -1,9 +1,15 @@
+import { auth } from "./firebase/firebase";
 import useCurrentUser from "./hooks/global/useCurrentUser";
 
 const App = () => {
   const currentUser = useCurrentUser();
 
-  return <p>{`Main app, welcome ${currentUser?.email}`}</p>;
+  return (
+    <>
+      <p>{`Main app, welcome ${currentUser?.uid}`}</p>
+      <button onClick={() => auth.signOut()}>Sign out</button>
+    </>
+  );
 };
 
 export default App;
