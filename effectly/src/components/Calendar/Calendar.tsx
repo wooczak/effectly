c260c385c114832fed43a97d6ba073b57a4a04c7
wooczak <kacper.luczak99@gmail.com>
2023-02-20 21:17:37 +0100

    Re-factoring Events components (1/2)

--- effectly/src/components/Calendar/Calendar.tsx
+++ effectly/src/components/Calendar/Calendar.tsx
@@ -1,14 +1,13 @@
 import { CalendarWrapper, EventsRows, NoEventsInfo } from "./Calendar.styles";
 
 import DatePicker from "./DatePicker/DatePicker";
-import Event from "./Event/Event";
+import Events from "./Event/Event";
 
 import useCalendarData from "../../hooks/Calendar/useCalendarData";
 import useDatePick from "../../hooks/Calendar/useDatePick";
 
 import { Calendar as CalendarVars } from "../../core/variables/variables";
-import { convertDate, filterEvents, getEventsMath } from "./helpers/events";
-import getRandomColor from "./helpers/getRandomColor";
+import { filterEvents } from "./helpers/events";
 import { Globals } from "../../core/variables/variables";
 
 type CalendarProps = {
@@ -51,32 +50,7 @@ const Calendar = ({ className, userId }: CalendarProps) => {
             </NoEventsInfo>
           ) : (
             <EventsRows>
-              {events.map((event, index) => {
-                const {
-                  event_start: start,
-                  event_end: end,
-                  event_name: name,
-                  event_location: location,
-                } = event;
-
-                const {
-                  intervalInMinutes: fromMidnight,
-                  durationInMinutes: duration,
-                } = getEventsMath(visibleDay, start, end);
-
-                return (
-                  <Event
-                    index={index}
-                    name={name}
-                    start={convertDate(start)}
-                    end={convertDate(end)}
-                    location={location}
-                    fromMidnight={fromMidnight}
-                    duration={duration}
-                    backgroundColor={getRandomColor()}
-                  />
-                );
-              })}
+              <Events data={events} visibleDay={visibleDay} />
             </EventsRows>
           )}
         </>
--- effectly/src/components/Calendar/Event/Event.tsx
+++ effectly/src/components/Calendar/Event/Event.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { getEventsMath } from "../helpers/events";
+import getRandomColor from "../helpers/getRandomColor";
 import { EventWrapper, Date, EventInfo } from "./Event.styles";
 
 interface EventProps {
-  index: any;
   name: string;
   location: string;
   start: string;
@@ -12,30 +13,33 @@ interface EventProps {
   duration: number;
 }
 
-const Event = ({
-  index,
-  name,
-  location,
-  start,
-  end,
-  fromMidnight,
-  duration,
-  backgroundColor,
-}: EventProps) => {
-  return (
-    <React.Fragment key={index}>
-      <EventWrapper fromMidnight={`${fromMidnight / 5}px`}>
-        <Date>{`${start} - ${end}`}</Date>
-        <EventInfo
-          backgroundColor={backgroundColor}
-          duration={`${duration / 7.5}px`}
-        >
-          <p className="event-name">{name}</p>
-          <p className="event-location">{location}</p>
-        </EventInfo>
-      </EventWrapper>
-    </React.Fragment>
-  );
+const Events = (data: any[], visibleDay: Date) => {
+  <React.Fragment>
+    {data.map((event, index) => {
+      const {
+        event_start: start,
+        event_end: end,
+        event_name: name,
+        event_location: location,
+      } = event;
+
+      const { intervalInMinutes: fromMidnight, durationInMinutes: duration } =
+        getEventsMath(visibleDay, start, end);
+
+      return (
+        <EventWrapper fromMidnight={`${fromMidnight / 5}px`} key={index}>
+          <Date>{`${start} - ${end}`}</Date>
+          <EventInfo
+            backgroundColor={getRandomColor()}
+            duration={`${duration / 7.5}px`}
+          >
+            <p className="event-name">{name}</p>
+            <p className="event-location">{location}</p>
+          </EventInfo>
+        </EventWrapper>
+      );
+    })}
+  </React.Fragment>;
 };
 
-export default Event;
+export default Events;
